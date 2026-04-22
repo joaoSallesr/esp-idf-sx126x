8fefbe4f90f301aaaab941cd59d12e96735d7afc
nopnop2002 <>
2026-04-23 04:42:27 +0900

    Added web-form project

---
+++ web-form/README.md
@@ -0,0 +1,87 @@
+# Web Form Example   
+Data entered through the web form is sent to LoRa.   
+Data received from LoRa is displayed in a web form.   
+```
+           +-----------+              +-----------+              +-----------+
+           |           |              |           |              |           |
+           |  WebForm  |-(WebSocket)->|   ESP32   |----(SPI)---->|  SX126x   |==(LoRa)==>
+           |           |              |           |              |           |
+           +-----------+              +-----------+              +-----------+
+
+           +-----------+              +-----------+              +-----------+
+           |           |              |           |              |           |
+==(LoRa)==>|  SX126x   |----(SPI)---->|   ESP32   |-(WebSocket)->|  WebForm  |
+           |           |              |           |              |           |
+           +-----------+              +-----------+              +-----------+
+```
+
+I used [this](https://github.com/Molorius/esp32-websocket) component.   
+This component can communicate directly with the browser.   
+It's a great job.   
+<img width="844" height="779" alt="Image" src="https://github.com/user-attachments/assets/eec0862a-d599-4b93-ada2-1844a2369ff5" />
+
+# Configuration
+<img width="659" height="486" alt="Image" src="https://github.com/user-attachments/assets/45a3cfaf-200b-4740-b4b8-5ef5107ff8da" />
+<img width="659" height="486" alt="Image" src="https://github.com/user-attachments/assets/1a72ee18-a18a-4c8e-9fc4-2612622a310f" />
+
+## WiFi Setting
+Set the information of your access point.   
+<img width="659" height="486" alt="Image" src="https://github.com/user-attachments/assets/12e1e0ef-6b22-42d6-b2b8-3ebfffec4b71" />
+
+## Radio Setting
+Set the wireless communication direction.   
+
+### Web to LoRa
+Data entered through the web form is sent to LoRa.   
+```
+           +-----------+              +-----------+              +-----------+
+           |           |              |           |              |           |
+           |  WebForm  |-(WebSocket)->|   ESP32   |----(SPI)---->|  SX126x   |==(LoRa)==>
+           |           |              |           |              |           |
+           +-----------+              +-----------+              +-----------+
+```
+
+<img width="659" height="486" alt="Image" src="https://github.com/user-attachments/assets/39da732e-e1d1-400b-a87f-18624c493f41" />
+
+
+### LoRa to Web
+Data received from LoRa is displayed in a web form.   
+
+```
+           +-----------+              +-----------+              +-----------+
+           |           |              |           |              |           |
+==(LoRa)==>|  SX126x   |----(SPI)---->|   ESP32   |-(WebSocket)->|  WebForm  |
+           |           |              |           |              |           |
+           +-----------+              +-----------+              +-----------+
+```
+
+<img width="659" height="486" alt="Image" src="https://github.com/user-attachments/assets/1ad7b03e-cc87-4968-aaa1-8902c8f64e41" />
+
+
+# Launch a web browser
+Enter the following in the address bar of your web browser.   
+```
+http:://{IP of ESP32}/
+or
+http://esp32-server.local/
+```
+
+<img width="844" height="779" alt="Image" src="https://github.com/user-attachments/assets/14e1d240-bbc3-4b79-bab3-e138b11e1c1f" />
+
+### Web to LoRa
+Enter the data to send in the TextBox and press the Send button.   
+<img width="844" height="779" alt="Image" src="https://github.com/user-attachments/assets/bc1f44f9-69fd-46af-a831-b0fd3f23bb0c" />
+
+### LoRa to Web
+The received data will be displayed in the TextBox.   
+The Change button changes the number of lines displayed.   
+The Copy button copies the received data to the clipboard.   
+<img width="844" height="779" alt="Image" src="https://github.com/user-attachments/assets/bcca0d64-3dff-47e2-a6c4-54a2b6b923de" />
+<img width="844" height="892" alt="Image" src="https://github.com/user-attachments/assets/b8462377-3640-4701-8ed1-07f4292925f9" />
+
+# WEB Pages
+WEB Pages are stored in the html folder.   
+I used [this](https://bulma.io/) open source css.   
+You cam change root.html as you like.   
+
+
---
+++ web-form/main/main.c
@@ -0,0 +1,334 @@
+/* The example of ESP-IDF
+ *
+ * This sample code is in the public domain.
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "freertos/message_buffer.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "mdns.h"
+#include "cJSON.h"
+
+#include "ra01s.h"
+#include "websocket_server.h"
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about one event */
+/* - are we connected to the AP with an IP? */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
+static const char *TAG = "MAIN";
+
+static int s_retry_num = 0;
+
+MessageBufferHandle_t xMessageBufferTrans;
+MessageBufferHandle_t xMessageBufferRecv;
+
+// The total number of bytes (not single messages) the message buffer will be able to hold at any one time.
+size_t xBufferSizeBytes = 1024;
+// The size, in bytes, required to hold each item in the message,
+size_t xItemSize = 255; // Maximum Payload size of SX1261/62/68 is 255
+
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+		esp_wifi_connect();
+	} else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+		if (s_retry_num < CONFIG_ESP_MAXIMUM_RETRY) {
+			esp_wifi_connect();
+			s_retry_num++;
+			ESP_LOGI(TAG, "retry to connect to the AP");
+		} else {
+			xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+		}
+		ESP_LOGI(TAG,"connect to the AP fail");
+	} else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+		ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+		ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+		s_retry_num = 0;
+		xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+	}
+}
+
+esp_err_t wifi_init_sta(void)
+{
+	s_wifi_event_group = xEventGroupCreate();
+
+	ESP_ERROR_CHECK(esp_netif_init());
+	ESP_ERROR_CHECK(esp_event_loop_create_default());
+	esp_netif_create_default_wifi_sta();
+
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+	esp_event_handler_instance_t instance_any_id;
+	esp_event_handler_instance_t instance_got_ip;
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+		ESP_EVENT_ANY_ID,
+		&event_handler,
+		NULL,
+		&instance_any_id));
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+		IP_EVENT_STA_GOT_IP,
+		&event_handler,
+		NULL,
+		&instance_got_ip));
+
+	wifi_config_t wifi_config = {
+		.sta = {
+			.ssid = CONFIG_ESP_WIFI_SSID,
+			.password = CONFIG_ESP_WIFI_PASSWORD,
+			/* Setting a password implies station will connect to all security modes including WEP/WPA.
+			 * However these modes are deprecated and not advisable to be used. Incase your Access point
+			 * doesn't support WPA2, these mode can be enabled by commenting below line */
+			.threshold.authmode = WIFI_AUTH_WPA2_PSK,
+
+			.pmf_cfg = {
+				.capable = true,
+				.required = false
+			},
+		},
+	};
+	ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
+	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+	ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+	ESP_ERROR_CHECK(esp_wifi_start());
+
+	/* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+	 * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+	esp_err_t ret_value = ESP_OK;
+	EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+		WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+		pdFALSE,
+		pdFALSE,
+		portMAX_DELAY);
+
+	/* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+	 * happened. */
+	if (bits & WIFI_CONNECTED_BIT) {
+		ESP_LOGI(TAG, "connected to ap SSID:%s password:%s", CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
+	} else if (bits & WIFI_FAIL_BIT) {
+		ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s", CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
+		ret_value = ESP_FAIL;
+	} else {
+		ESP_LOGE(TAG, "UNEXPECTED EVENT");
+		ret_value = ESP_FAIL;
+	}
+
+	/* The event will not be processed after unregister */
+	ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+	ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+	vEventGroupDelete(s_wifi_event_group);
+	return ret_value;
+}
+
+void initialise_mdns(void)
+{
+	//initialize mDNS
+	ESP_ERROR_CHECK( mdns_init() );
+	//set mDNS hostname (required if you want to advertise services)
+	ESP_ERROR_CHECK( mdns_hostname_set(CONFIG_MDNS_HOSTNAME) );
+	ESP_LOGI(TAG, "mdns hostname set to: [%s]", CONFIG_MDNS_HOSTNAME);
+
+	//initialize service
+	ESP_ERROR_CHECK( mdns_service_add(NULL, "_http", "_tcp", 80, NULL, 0) );
+#if 0
+	//set default mDNS instance name
+	ESP_ERROR_CHECK( mdns_instance_name_set("ESP32 with mDNS") );
+#endif
+}
+
+#if CONFIG_SENDER
+void task_tx(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	uint8_t buf[xItemSize];
+	while(1) {
+		size_t received = xMessageBufferReceive(xMessageBufferRecv, buf, sizeof(buf), portMAX_DELAY);
+		ESP_LOGI(pcTaskGetName(NULL), "xMessageBufferReceive received=[%.*s]", received, buf);
+
+		// Wait for transmission to complete
+		ESP_LOGD(pcTaskGetName(NULL), "Transmission begin");
+		if (LoRaSend(buf, received, SX126x_TXMODE_SYNC) == false) {
+			ESP_LOGE(pcTaskGetName(NULL),"LoRaSend fail");
+		}
+
+		// Do not wait for the transmission to be completed
+		//LoRaSend(buf, received, SX126x_TXMODE_ASYNC );
+
+		// Get packet loss count
+		int lost = GetPacketLost();
+		if (lost != 0) {
+			ESP_LOGW(pcTaskGetName(NULL), "%d packets lost", lost);
+		}
+		ESP_LOGD(pcTaskGetName(NULL), "Transmission done");
+	} // end while
+	vTaskDelete(NULL);
+}
+#endif // CONFIG_SENDER
+
+#if CONFIG_RECEIVER
+void task_rx(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	uint8_t buf[xItemSize];
+	while(1) {
+		uint8_t rxLen = LoRaReceive(buf, sizeof(buf)-1);
+		if ( rxLen > 0 ) { 
+			ESP_LOGI(pcTaskGetName(NULL), "%d byte packet received:[%.*s]", rxLen, rxLen, buf);
+
+			int8_t rssi, snr;
+			GetPacketStatus(&rssi, &snr);
+			ESP_LOGI(pcTaskGetName(NULL), "rssi=%d[dBm] snr=%d[dB]", rssi, snr);
+
+			buf[rxLen] = 0;
+			cJSON *root;
+			root = cJSON_CreateObject();
+			cJSON_AddStringToObject(root, "id", "recv-request");
+			cJSON_AddStringToObject(root, "payload", (char *)buf);
+			//char *my_json_string = cJSON_Print(root);
+			char *my_json_string = cJSON_PrintUnformatted(root);
+			int rxLen = strlen(my_json_string);
+			ESP_LOGD(pcTaskGetName(NULL), "my_json_string=[%s]",my_json_string);
+			cJSON_Delete(root);
+
+			size_t spacesAvailable = xMessageBufferSpacesAvailable( xMessageBufferTrans );
+			ESP_LOGI(pcTaskGetName(NULL), "spacesAvailable=%d", spacesAvailable);
+			size_t sended = xMessageBufferSend(xMessageBufferTrans, my_json_string, rxLen, 100);
+			if (sended != rxLen) {
+				ESP_LOGE(pcTaskGetName(NULL), "xMessageBufferSend fail rxLen=%d sended=%d", rxLen, sended);
+				break;
+			}
+			cJSON_free(my_json_string);
+		}
+		vTaskDelay(1); // Avoid WatchDog alerts
+	} // end while
+	vTaskDelete(NULL);
+}
+
+void task_dummy(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	uint8_t buf[xItemSize];
+	while(1) {
+		size_t received = xMessageBufferReceive(xMessageBufferRecv, buf, sizeof(buf), portMAX_DELAY);
+		ESP_LOGI(pcTaskGetName(NULL), "xMessageBufferReceive received=[%.*s]", received, buf);
+	} // end while
+	vTaskDelete(NULL);
+}
+#endif // CONFIG_RECEIVER
+
+void client_task(void* pvParameters);
+void server_task(void* pvParameters);
+
+void app_main()
+{
+	// Initialize NVS
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
+
+	// Initialize WiFi
+	ESP_ERROR_CHECK(wifi_init_sta());
+
+	// Create MessageBuffer
+	xMessageBufferTrans = xMessageBufferCreate(xBufferSizeBytes);
+	configASSERT( xMessageBufferTrans );
+	xMessageBufferRecv = xMessageBufferCreate(xBufferSizeBytes);
+	configASSERT( xMessageBufferRecv );
+
+	// Get the local IP address
+	esp_netif_ip_info_t ip_info;
+	ESP_ERROR_CHECK(esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_STA_DEF"), &ip_info));
+	char cparam0[64];
+	sprintf(cparam0, IPSTR, IP2STR(&ip_info.ip));
+	//sprintf(cparam0, "%s.local", CONFIG_MDNS_HOSTNAME);
+
+	// Initialize mdns
+	initialise_mdns();
+
+	// Initialize LoRa
+	LoRaInit();
+	int8_t txPowerInDbm = 22;
+
+	uint32_t frequencyInHz = 0;
+#if CONFIG_433MHZ
+	frequencyInHz = 433000000;
+	ESP_LOGI(TAG, "Frequency is 433MHz");
+#elif CONFIG_866MHZ
+	frequencyInHz = 866000000;
+	ESP_LOGI(TAG, "Frequency is 866MHz");
+#elif CONFIG_915MHZ
+	frequencyInHz = 915000000;
+	ESP_LOGI(TAG, "Frequency is 915MHz");
+#elif CONFIG_OTHER
+	ESP_LOGI(TAG, "Frequency is %dMHz", CONFIG_OTHER_FREQUENCY);
+	frequencyInHz = CONFIG_OTHER_FREQUENCY * 1000000;
+#endif
+
+#if CONFIG_USE_TCXO
+	ESP_LOGW(TAG, "Enable TCXO");
+	float tcxoVoltage = 3.3; // use TCXO
+	bool useRegulatorLDO = true; // use DCDC + LDO
+#else
+	ESP_LOGW(TAG, "Disable TCXO");
+	float tcxoVoltage = 0.0;  // don't use TCXO
+	bool useRegulatorLDO = false;  // use only LDO in all modes
+#endif
+
+	//LoRaDebugPrint(true);
+	if (LoRaBegin(frequencyInHz, txPowerInDbm, tcxoVoltage, useRegulatorLDO) != 0) {
+		ESP_LOGE(TAG, "Does not recognize the module");
+		while(1) {
+			vTaskDelay(1);
+		}
+	}
+	
+	uint8_t spreadingFactor = 7;
+	uint8_t bandwidth = 4;
+	uint8_t codingRate = 1;
+	uint16_t preambleLength = 8;
+	uint8_t payloadLen = 0;
+	bool crcOn = true;
+	bool invertIrq = false;
+#if CONFIG_ADVANCED
+	spreadingFactor = CONFIG_SF_RATE;
+	bandwidth = CONFIG_BANDWIDTH;
+	codingRate = CONFIG_CODING_RATE;
+#endif
+	LoRaConfig(spreadingFactor, bandwidth, codingRate, preambleLength, payloadLen, crcOn, invertIrq);
+
+	// Start web socket server
+	ws_server_start();
+
+	// Start web server
+	xTaskCreate(&server_task, "server_task", 1024*2, (void *)cparam0, 5, NULL);
+
+	// Start web client
+	xTaskCreate(&client_task, "client_task", 1024*4, NULL, 5, NULL);
+
+#if CONFIG_SENDER
+	xTaskCreate(&task_tx, "TX", 1024*4, NULL, 5, NULL);
+#endif
+#if CONFIG_RECEIVER
+	xTaskCreate(&task_rx, "RX", 1024*4, NULL, 5, NULL);
+	xTaskCreate(&task_dummy, "DUMMY", 1024*4, NULL, 5, NULL);
+#endif
+}
+
